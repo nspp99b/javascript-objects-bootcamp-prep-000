d13bf8a8fc14e4d1e02b76ac4ded45ee23d1ae25
Shahin Motia <chromepanthers@gmail.com>
2017-10-09 04:02:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = new Object({ CCR: 'Cotton Fields' })
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist["artistName"] = "songTitle"
+  playlist['artistName'] = 'songTitle'
   return playlist
 }
